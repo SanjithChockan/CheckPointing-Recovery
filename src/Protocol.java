5613e4392502759b18c3a8836c7e67df8bcaff48
Sanjith Chockan <sanjith.chockan@gmail.com>
2023-11-21 22:51:20 -0700

    defined classes/threads needed to send messages and accounting freezing

--- src/Protocol.java
+++ src/Protocol.java
@@ -10,12 +10,14 @@
  */
 
 import java.util.HashMap;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class Protocol {
 
     Node currentNode;
     HashMap<Integer, Integer> FLS;
     HashMap<Integer, Integer> LLR;
+    ReentrantLock sendMessageLock = new ReentrantLock(Boolean.TRUE);
 
     public Protocol(Node currentNode) {
         this.currentNode = currentNode;
@@ -24,19 +26,27 @@ public class Protocol {
     }
 
     public void initializeFLS() {
-        for (Node neighbor: currentNode.neighbors) {
+        for (Node neighbor : currentNode.neighbors) {
             FLS.put(neighbor.ID, Integer.MIN_VALUE);
         }
     }
 
     public void initializeLLR() {
-        for (Node neighbor: currentNode.neighbors) {
-            FLS.put(neighbor.ID, Integer.MIN_VALUE);
+        for (Node neighbor : currentNode.neighbors) {
+            LLR.put(neighbor.ID, Integer.MIN_VALUE);
         }
+    }
+
+    public void receiveMessages() {
+        
     }
 
     /*
      * CHECKPOINT ALGORITHM
+     * To send messages (application, ck, etc.) have different threads and only send
+     * when a lock is aquired
+     * Will help freeze application message until lock is given up by checkpointing
+     * thread
      * - Take tentative checkpoint
      * - Request checkpoints to its cohorts
      * - Wait for yes from all cohorts
@@ -44,4 +54,39 @@ public class Protocol {
      * - else discard tentative checkpoint
      */
 
+    class Checkpoint implements Runnable {
+
+        @Override
+        public void run() {
+            // TODO Auto-generated method stub
+            takeTentativeCK();
+        }
+
+        public void takeTentativeCK() {
+            // take local checkpoint
+            // then send request to cohorts
+        }
+
+        public void sendRequestToCohorts() {
+
+            // await until you receive decision
+
+        }
+    }
+
+    // this will keep running to send APP messages
+    // only way it will freeze is when current process obtains sendMessage lock to
+    // take tentative checkpoint
+    class Application implements Runnable {
+
+        @Override
+        public void run() {
+            // TODO Auto-generated method stub
+            sendApplicationMessages();
+        }
+
+        public void sendApplicationMessages() {
+
+        }
+    }
 }
